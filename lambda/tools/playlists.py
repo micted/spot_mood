75f070530aafe52440e8958cda61ec26d51dd080
micted <micdem94@gmail.com>
2023-02-27 07:33:10 +0100

    modified playlist

--- lambda/tools/playlists.py
+++ lambda/tools/playlists.py
@@ -29,8 +29,8 @@ items = response.get("Items")
 # If the table is empty or the access token is not found, return an error response
 if not items:
     print("DynamoDB table is empty")
-    return {"statusCode": 500, "body": {"error": "access_token item not found in DynamoDB table"}}
-
+    error = {"statusCode": 500, "body": {"error": "access_token item not found in DynamoDB table"}}
+    print(error)git
 print(items)
 #access_token_value = last_item['S']
 latest_timestamp = 0
